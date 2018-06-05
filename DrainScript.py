750e228f9c31b2b0a2c3e843faa6bc2efd2fb3ee
Andrew-J-Miller <32602661+Andrew-J-Miller@users.noreply.github.com>
2018-06-05 00:26:03 -0700

    Add files via upload

---
+++ DrainScript.py
@@ -0,0 +1,59 @@
+import RPi.GPIO as GPIO
+import time
+import sys
+import os
+import tkinter as tk
+from tkinter import ttk
+import datetime
+
+#libraries for thermocouple ADC and GPIO SPI
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_MAX31855.MAX31855 as MAX31855
+
+
+
+#Definition for child process that will run to display current temperature
+def child(msg):
+    popup = tk.Tk()
+
+
+    def leavemini():
+        popup.destroy()
+
+
+    popup.wm_title("Running Boil")
+    label = ttk.Label(popup, text=msg)
+    label.pack(side="top", fill="x", pady=10)
+    B1=ttk.Button(popup, text="Stop", command = leavemini)
+    B1.pack()
+
+
+    popup.mainloop()
+
+	
+# Raspberry Pi hardware SPI configuration.
+SPI_PORT   = 0
+SPI_DEVICE = 0
+sensor = MAX31855.MAX31855(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
+
+newpid = os.fork()
+if newpid == 0:
+    child("Draining wort")
+	GPIO.cleanup()
+    os.system("pkill -f MashScript.py")
+    sys.exit()
+
+#Setting up GPIO pins
+GPIO.setmode(GPIO.BCM)	
+
+#pin for activating pump relay currently set to 17 but may need to be changed later
+GPIO.setup(26, GPIO.OUT)
+#pin for activating solenoid valve
+GPIO.setup(25, GPIO.OUT)
+
+while True:
+	#write solenoid valve to high
+	GPIO.output(25, GPIO.HIGH)
+	#write the pump relay pin to high to activate the pump
+	GPIO.output(26, GPIO.HIGH)
+		
