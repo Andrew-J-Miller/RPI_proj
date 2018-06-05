64a06285899e67793d11658eb5bc1d7c584d3f1b
Andrew-J-Miller <32602661+Andrew-J-Miller@users.noreply.github.com>
2018-06-05 05:12:37 -0700

    Update simpletest.py

--- simpletest.py
+++ simpletest.py
@@ -39,15 +39,15 @@ def c_to_f(c):
 # software or hardware SPI.
 
 # Raspberry Pi software SPI configuration.
-CLK = 5
-CS  = 6
-DO  = 13
-sensor = MAX31855.MAX31855(CLK, CS, DO)
+#CLK = 5
+#CS  = 6
+#DO  = 13
+#sensor = MAX31855.MAX31855(CLK, CS, DO)
 
 # Raspberry Pi hardware SPI configuration.
-#SPI_PORT   = 0
-#SPI_DEVICE = 0
-#sensor = MAX31855.MAX31855(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
+SPI_PORT   = 0
+SPI_DEVICE = 0
+sensor = MAX31855.MAX31855(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
 # BeagleBone Black software SPI configuration.
 #CLK = 'P9_12'
@@ -63,7 +63,7 @@ sensor = MAX31855.MAX31855(CLK, CS, DO)
 # Loop printing measurements every second.
 print('Press Ctrl-C to quit.')
 while True:
-    temp = sensor.readLinearizedTempC()
+    temp = sensor.readTempC()
     internal = sensor.readInternalC()
     print('Thermocouple Temperature: {0:0.3F}*C / {1:0.3F}*F'.format(temp, c_to_f(temp)))
     print('    Internal Temperature: {0:0.3F}*C / {1:0.3F}*F'.format(internal, c_to_f(internal)))
